60bc843f124f32842b777310efa8179ad64972c2
Andy <atreto@minimum>
2024-04-07 13:09:35 -0400

    adding more mini modules, and cleaning up init

--- init.lua
+++ init.lua
@@ -1,25 +1,25 @@
- local rocks_config = {
-     rocks_path = "/home/atreto/.local/share/rock/rocks",
-     luarocks_binary = "/home/atreto/.local/share/rock/rocks/bin/luarocks",
-     -- rocks_path = vim.fn.stdpath("data") .. "/" .. os.getenv("$NVIM_APPNAME") .. "/rocks",
-     -- luarocks_binary = "luarocks",
- }
+local rocks_config = {
+    rocks_path = "/home/atreto/.local/share/rock/rocks",
+    luarocks_binary = "/home/atreto/.local/share/rock/rocks/bin/luarocks",
+    -- rocks_path = vim.fn.stdpath("data") .. "/" .. os.getenv("NVIM_APPNAME") .. "/rocks",
+    -- luarocks_binary = "luarocks",
+}
 
- vim.g.rocks_nvim = rocks_config
+vim.g.rocks_nvim = rocks_config
 
- local luarocks_path = {
-     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
- }
- package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+local luarocks_path = {
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+}
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
- local luarocks_cpath = {
-     vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-     vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
- }
- package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+local luarocks_cpath = {
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+}
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
- vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
+vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- plugins/mini.lua
+++ plugins/mini.lua
@@ -9,6 +9,8 @@ require("mini.starter").setup()
 require("mini.sessions").setup()
 require("mini.pairs").setup()
 require("mini.indentscope").setup()
+require("mini.tabline").setup()
+require("mini.bracketed").setup()
 
 vim.keymap.set('n', '<leader>.', function() MiniFiles.open(vim.api.nvim_buf_get_name(0), false) end, { desc = "files CWD"})
 vim.keymap.set('n', '<leader>gd', function() MiniDiff.toggle_overlay(0) end, { desc = "toggle Git Diff Overlay"})
