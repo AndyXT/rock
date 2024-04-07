3a521b942685a3fb60f6e8e5175de723b612252b
Andy <atreto@minimum>
2024-04-07 11:41:08 -0400

    adding some comments to make this config more generic

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
  local rocks_config = {
      rocks_path = "/home/atreto/.local/share/rock/rocks",
      luarocks_binary = "/home/atreto/.local/share/rock/rocks/bin/luarocks",
+     -- rocks_path = vim.fn.stdpath("data") .. "/rocks",
+     -- luarocks_binary = "luarocks",
  }
 
  vim.g.rocks_nvim = rocks_config
